857141b8fd51cccfe19833e07e2fe6afc9b4aaa3
OluchiC <Oluchi.j.c@Gmail.com>
2019-09-29 07:30:21 -0400

    interest form

---
+++ InterestForm/interestFormJS.js
@@ -0,0 +1,27 @@
+
+function setVisibility(id) {
+    if(document.getElementById('bt1').value=='Hide Layer'){
+    document.getElementById('bt1').value = 'Show Layer';
+    document.getElementById(id).style.display = 'none';
+    }else{
+    document.getElementById('bt1').value = 'Hide Layer';
+    document.getElementById(id).style.display = 'inline';
+    }
+}
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function setVisibility(id) {
+    if(document.getElementById('bt1').value=='Hide Layer'){
+    document.getElementById('bt1').value = 'Show Layer';
+    document.getElementById(id).style.display = 'none';
+    }else{
+    document.getElementById('bt1').value = 'Hide Layer';
+    document.getElementById(id).style.display = 'inline';
+    }
+}
